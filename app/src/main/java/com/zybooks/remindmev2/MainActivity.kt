e268172b11f41314ea7d1bf7020545b10a9b3d65
KevinBeltran23 <kevinbeltran2437@gmail.com>
2025-12-03 00:36:42 -0800

    fixed dark mode

--- app/src/main/java/com/zybooks/remindmev2/MainActivity.kt
+++ app/src/main/java/com/zybooks/remindmev2/MainActivity.kt
@@ -7,17 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
+import com.zybooks.remindmev2.ui.AppViewModelProvider
 import com.zybooks.remindmev2.ui.navigation.RemindMeNavGraph
 import com.zybooks.remindmev2.ui.theme.RemindMev2Theme
+import com.zybooks.remindmev2.ui.viewmodels.SettingsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RemindMev2Theme {
+            val settingsViewModel: SettingsViewModel = viewModel(factory = AppViewModelProvider.Factory)
+            val userSettings by settingsViewModel.uiState.collectAsState()
+
+            RemindMev2Theme(
+                darkTheme = userSettings.darkMode
+            ) {
                 val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     RemindMeNavGraph(
